f7f79d30c164634fba64fd87cd81c2f464655331
Josh Addington <jraddington@hotmail.com>
2021-09-08 14:48:49 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-provider "azurerm" {
+mprovider "azurerm" {
   features {}
 }
 
@@ -45,7 +45,7 @@ resource "azurerm_linux_virtual_machine" "example" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = "example"
   }
 
   os_disk {
